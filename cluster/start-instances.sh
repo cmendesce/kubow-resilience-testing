3e4e9123affde30f476b1150e0ec342a1249c710
Carlos Mendes <cmendesce@gmail.com>
2019-11-24 22:46:09 -0300

    Add cluster creation scripts

---
+++ cluster/start-instances.sh
@@ -0,0 +1,8 @@
+set -e
+
+IDS=$(aws ec2 describe-instances --output json | jq -r '.Reservations[].Instances | map(.InstanceId) | join(", ")')
+
+for id in $IDS
+do
+  aws ec2 start-instances --instance-id $id
+done
